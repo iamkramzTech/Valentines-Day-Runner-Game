ee1aad21b9129b227ec9f2e8f3642ceab8cef5a5
iamkramzTech <bernaldezm88@yahoo.com>
2024-02-13 23:13:37 +0800

    Created Obstacle Interface

---
+++ IObstacle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValentinesDay
+{
+    public interface IObstacle
+    {
+        PictureBox PictureBox { get; } // Represents the PictureBox control used for the obstacle
+        ObstacleType Type { get; } // Represents the type of obstacle
+        public ScoreManager scoreManager { get; }
+        
+        
+        bool CollidesWithCharacter(ICharacter character); // Method to check collision with a character 
+    }
+
+    public interface ISpeed
+    {
+        int ObstacleSpeed { get; }
+    }
+}
