1be4cd09521fa57ad628d2dc41ae6a80023461cd
iamkramzTech <bernaldezm88@yahoo.com>
2024-02-13 23:22:22 +0800

    Managing Score

---
+++ ScoreManager.cs
@@ -0,0 +1,23 @@
+﻿using static System.Formats.Asn1.AsnWriter;
+
+namespace ValentinesDay
+{
+    public class ScoreManager
+    {
+        private int _score;
+        public int Score=>_score;
+        public ScoreManager()
+        {
+            _score = 0;
+        }
+        public void IncrementScore(int amount)
+        {
+            _score += amount;
+        }
+        public void ResetScore()
+        {
+            _score = 0;
+        }
+
+    }
+}
