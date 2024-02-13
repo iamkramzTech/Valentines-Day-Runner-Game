ea5956d04d1ae2d6e5d8347691547642fcfbc6c2
iamkramzTech <bernaldezm88@yahoo.com>
2024-02-13 23:12:57 +0800

    Added Heart Obstacle

---
+++ Heart.cs
@@ -0,0 +1,27 @@
+﻿namespace ValentinesDay
+{
+    public class Heart : IObstacle
+    {
+        public PictureBox PictureBox { get; }
+        public ObstacleType Type => ObstacleType.Heart;
+        public ScoreManager scoreManager { get; }
+        public Heart(PictureBox pictureBox)
+        {
+            PictureBox = pictureBox;
+        }
+        public bool CollidesWithCharacter(ICharacter character)
+        {
+
+            if (PictureBox.Bounds.IntersectsWith(character.CharacterPictureBox.Bounds))
+            {
+                // Implement score increment logic here
+                // For example: score += 10;
+                scoreManager.IncrementScore(1);
+                return true; // Return true to indicate a collision has occurred
+            }
+            return false;
+
+        }
+        
+    }
+}
