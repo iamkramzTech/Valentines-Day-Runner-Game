ea1a0e3808ae86a46c04a0af67e47bab6ca91eff
iamkramzTech <bernaldezm88@yahoo.com>
2024-02-13 23:12:18 +0800

    Added Cactus Obstacle

---
+++ Cactus.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Windows.Forms;
+namespace ValentinesDay
+{
+    public class Cactus : IObstacle, ISpeed
+    {
+        public PictureBox PictureBox { get; }
+        private int _obstacleSpeed;
+        public int ObstacleSpeed => _obstacleSpeed;
+        public ObstacleType Type => ObstacleType.Cactus;
+
+        public ScoreManager scoreManager { get; }
+        private Form form;
+        private Random random;
+        public Cactus(PictureBox pictureBox)
+        {
+            PictureBox = pictureBox;
+          //  _obstacleSpeed = obstacleSpeed;
+        }
+        public bool CollidesWithCharacter(ICharacter character)
+        {
+
+            if (PictureBox.Bounds.IntersectsWith(character.CharacterPictureBox.Bounds))
+            {
+                MessageBox.Show("Character Die");
+                return true;
+            }
+
+            return false;
+        }
+
+        public void IncreaseSpeed(int amount)
+        {
+
+            if (scoreManager.Score > 5)
+            {
+                _obstacleSpeed += amount;
+            }
+        }
+    }
+}
